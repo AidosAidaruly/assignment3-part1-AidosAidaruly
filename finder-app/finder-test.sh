8e06ffc07d73ba5cabafb3d5a4be60e604e719fd
AidosAidaruly <aidosfiyoy677@gmail.com>
2026-01-14 15:08:07 +0000

    Trigger actions

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -1,12 +1,15 @@
 #!/bin/sh
 # Tester script for assignment 1 and assignment 2
 # Author: Siddhant Jajoo
+
 set -e
 set -u
+
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
 username=$(cat conf/username.txt)
+
 if [ $# -lt 3 ]
 then
         echo "Using default value ${WRITESTR} for string to write"
@@ -21,14 +24,20 @@ else
         WRITESTR=$2
         WRITEDIR=/tmp/aeld-data/$3
 fi
+
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
+
 echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
+
 rm -rf "${WRITEDIR}"
+
 # create $WRITEDIR if not assignment1
 assignment=`cat conf/assignment.txt`
+
 if [ $assignment != 'assignment1' ]
 then
         mkdir -p "$WRITEDIR"
+        
         if [ -d "$WRITEDIR" ]
         then
                 echo "$WRITEDIR created"
@@ -36,14 +45,19 @@ then
                 exit 1
         fi
 fi
+
 echo "Removing the old writer utility and compiling as a native application"
+
 for i in $( seq 1 $NUMFILES)
 do
         ./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
+
 OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+
 # remove temporary directories
 rm -rf /tmp/aeld-data
+
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
